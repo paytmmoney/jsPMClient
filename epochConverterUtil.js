20370d69fff3fd1714291709e1a7f150f1e9157f
anupam.baranwal <anupam.baranwal@paytmmoney.com>
2023-03-14 14:21:47 +0530

    added ninetyEightyConstant

--- epochConverterUtil.js
+++ epochConverterUtil.js
@@ -1,7 +1,9 @@
 function EpochConverter(time){
-    const ninetyEighty = new Date(Date.UTC(1980, 0, 1, 0, 0, 0));
-    const timenew = Math.floor(ninetyEighty.getTime() / 1000);
-    const output_epoch_value = time + timenew;
+    // Value of ninetyEightyConstant is derived by -
+    // const ninetyEighty = new Date(Date.UTC(1980, 0, 1, 0, 0, 0));
+    // const ninetyEightyConstant = Math.floor(ninetyEighty.getTime() / 1000);
+    ninetyEightyConstant = 315532800
+    const output_epoch_value = time + ninetyEightyConstant;
     return output_epoch_value
 }
 
